9bd80b02050dbf1ae123ae3793a8d7ad19cbe88d
rapstech <sonu@rapscspl.com>
2018-06-27 09:33:27 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {UserModule} from './user/user.module';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './user/login/login.component';
+
+const route : Routes = [
+  { path : '', component : LoginComponent},
+  
+ 
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(route),UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
